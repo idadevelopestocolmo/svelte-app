c6d7a39c703c3f1a7dff0a9015c3f086dd0e3c69
ida.sylven <ida.sylven@sogeti.com>
2022-01-30 13:47:44 +0100

    Broken img deploy for question on SO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,7 +19,7 @@
 </svelte:head>
 
 <body class="bg-gray-800"></body>
-<!-- <img class="my-8 mb-10 mx-auto" src="src\assets\tvm-header-logo.png" alt="Tv Maze logo"> -->
+<img class="my-8 mb-10 mx-auto" src="src\assets\tvm-header-logo.png" alt="Tv Maze logo">
 <input class="w-full rounded-md mt-12 text-lg p-4 border-2 border-orange-500 mb-6 bg-gray-700 text-teal-500 placeholder:text-teal-500" type="text" bind:value={searchShow} placeholder="Search show..." spellcheck="false">
 <div class="pyu-4 grid gap-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
     {#each filteredShow as singleShow}
